a477a448f62a52cc5053ac29996106a39a4b275c
Leonardo Muñoz <54684005+neoxnaru@users.noreply.github.com>
2022-01-14 08:16:42 -0600

    Add files via upload

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,138 @@
+﻿using CrudPrueba.Data;
+using CrudPrueba.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudPrueba.Controllers
+{
+    public class ClientesController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+
+        //Http Index
+        public IActionResult Index()
+        {
+            IEnumerable<Cliente> ListClientes = _context.Clientes;
+
+            return View(ListClientes);
+        }
+
+        //Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        //Http Post Create
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El Cliente se creo correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        //Http Get Edit
+        public IActionResult Edit(int? Codigo)
+        {
+
+            if (Codigo == null || Codigo == 0) 
+            {
+                return NotFound();
+            }
+
+            var clientes = _context.Clientes.Find(Codigo);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+            return View(clientes);
+
+        }
+
+        //Http Post Edit
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Update(cliente);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El Cliente se actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        //Http Get Delete
+        public IActionResult Delet(int? Codigo)
+        {
+
+            if (Codigo == null || Codigo == 0)
+            {
+                return NotFound();
+            }
+
+            var clientes = _context.Clientes.Find(Codigo);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+            return View(clientes);
+
+        }
+
+        //Http Post Delet
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult DeleteClientes(int? Codigo)
+        {
+           
+            var clientes = _context.Clientes.Find(Codigo);
+
+            if (clientes== null)
+            {
+                return NotFound();
+
+            }
+
+
+            _context.Clientes.Remove(clientes);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El Cliente se eliminado correctamente";
+                return RedirectToAction("Index");
+           
+
+    
+
+        }
+
+
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudPrueba.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Required(ErrorMessage = " El Codigo es obligatorio")]
+        public int Codigo{ get; set; }
+
+        [Required(ErrorMessage =" El Nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Display(Name = "Estado Civil")]
+        public string EstadoCivil { get; set; }
+
+        [Display(Name = "Este Campo se refiere si esta vigente o no")]
+        public bool Estado { get; set; }
+
+    }
+}
